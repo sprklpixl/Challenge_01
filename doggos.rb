709e4fdd76d1d83a6df063af2e7eb906c5e63924
sprklpixl <shandell@tutamail.com>
2024-05-07 10:45:29 -0500

    assignment starter files

---
+++ doggos.rb
@@ -0,0 +1,9 @@
+require 'net/http'
+require 'json'
+require 'pp'
+
+url = 'https://dog.ceo/api/breeds/list/all'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+dog_breeds = JSON.parse(response) # Convert JSON data into Ruby data.
+pp dog_breeds # pp stands for pretty print.
---
+++ ghosts.rb
@@ -0,0 +1,6 @@
+ghosts.each do |ghost|
+  ghost_info  = "#{ghost[:name]} is #{ghost[:age]} years old, "
+  ghost_info += "loves #{ghost[:loves]} and "
+  ghost_info += "has #{ghost[:net_worth]} dollars in the bank."
+  puts ghost_info
+end
