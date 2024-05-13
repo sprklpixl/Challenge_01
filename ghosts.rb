bd38579637c5322c9c24a212e22f4cc5141cad5d
sprklpixl <shandell@tutamail.com>
2024-05-13 09:35:41 -0500

    carl sagan complete

--- ghosts.rb
+++ ghosts.rb
@@ -1,3 +1,10 @@
+ghosts = {
+  :name => ['Inky', 'Pinky', 'Blinky', 'Clyde']
+  :age => [4, 5, 6, 7]
+  :loves => ['reindeers', 'garden', 'ninjas', 'yarn']
+  :net_worth => [0, 14, 18.03, 25]
+}
+
 ghosts.each do |ghost|
   ghost_info  = "#{ghost[:name]} is #{ghost[:age]} years old, "
   ghost_info += "loves #{ghost[:loves]} and "
